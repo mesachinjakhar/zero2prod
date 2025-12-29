3399a1aa24837fdd514a93e660fbba243172a208
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-29 14:53:13 +0530

    tracing span

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,18 @@ pub struct FormData {
 
 pub async fn subscibe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+
+    let request_span = tracing::info_span!("Add a new subscriber", %request_id, subsciber_email = %form.email, subsciber_name = %form.name);
+    let _request_span_guard = request_span.enter();
+
+
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id.form.email,
+        request_id,
+        form.email,
         form.name
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
@@ -33,11 +39,14 @@ pub async fn subscibe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Htt
     .await
     {
         Ok(_) => {
-            log::info!("request_id {} - New subscriber details have been saved", request_id);
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("request_id {} - Failed to execute query{:?}", request_id, e);
+            tracing::error!("request_id {} - Failed to execute query{:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
