e6057c5c6bbb24d9594af14b534d2477fd4b2cce
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-29 17:30:51 +0530

    logtracer

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use zero2prod::startup::run;
 use tracing::subscriber::{self, set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
     let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
