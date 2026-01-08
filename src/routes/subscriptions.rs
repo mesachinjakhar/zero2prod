c6446ca2999e6938bfc2a4748ce0e5c145d6d504
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-08 13:37:02 +0530

    instrument added

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,7 @@
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
-use log::info;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,8 +12,20 @@ pub struct FormData {
 
 pub async fn subscibe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!("request_id {} - Adding '{}' '{}' as a new subscriber", request_id, form.email, form.name);
-    log::info!("request_id {} - Saving new subsciber details in the database", request_id);
+    let request_span = tracing::info_span!(
+        "Adding a new subsciber",
+        %request_id, 
+        subsciber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+
+    let query_span = tracing::info_span!("Saving new subsciber details in the database");
+
+
+    tracing::info!("request_id {} - Saving new subsciber details in the database", request_id);
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) 
     VALUES ($1, $2, $3, $4)
@@ -24,14 +36,15 @@ pub async fn subscibe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Htt
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!("request id {} - New subsciber details have been saved", request_id);
+            tracing::info!("request id {} - New subsciber details have been saved", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("request id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("request id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         },
     }
